daa6b82c416a831c0e33a12897ab7fe35cc7842d
Veronika Miljakovic <veronika.miljakovic1996@gmail.com>
2024-02-28 22:53:08 +0100

    Dodate sql skripte za kreiranje baze -nakon toga kad pokrenete aplikaciju hibernate ce napraviti sve tabele - dodata i skripta za insertovanje redova u bazu

---
+++ src/main/resources/sql/kreiraj_bazu.sql
@@ -0,0 +1,2 @@
+create database form;
+use form;
---
+++ src/main/resources/sql/popuni_tabele.sql
@@ -0,0 +1,12 @@
+insert into formular values (1, "formular1", "2024-02-28 22:29:46.150639", "2024-02-28 22:29:46.150639");
+insert into formular values (2, "formular2", "2024-02-28 22:29:46.150639", "2024-02-28 22:29:46.150639");
+insert into polje values(1, "polje1", 0, "TEKST", "2024-02-28 22:29:46.150639", "2024-02-28 22:29:46.150639", 1);
+insert into polje values(2, "polje2", 0, "BROJ", "2024-02-28 22:29:46.150639", "2024-02-28 22:29:46.150639", 2);
+insert into formular_popunjen values (1, "2024-02-28 22:29:46.150639", "2024-02-28 22:29:46.150639", 1);
+insert into formular_popunjen values (2, "2024-02-28 22:29:46.150639", "2024-02-28 22:29:46.150639", 2);
+insert into polje_popunjeno values (1,1,null,"Tekst","2024-02-28 22:29:46.150639", "2024-02-28 22:29:46.150639", 1);
+insert into polje_popunjeno values (2,2,null,"Broj","2024-02-28 22:29:46.150639", "2024-02-28 22:29:46.150639", 2);
+insert into korisnik values (1, "korisnik1", "$2a$10$sA6a60z9jbDc8WsVWIjQ8eyrOJBW84WMhx16OxZQ9t/KqQF/e7SIm", "2024-02-28 22:29:46.150639", "2024-02-28 22:29:46.150639");
+insert into korisnik values (2, "korisnik2", "$2a$10$sA6a60z9jbDc8WsVWIjQ8eyrOJBW84WMhx16OxZQ9t/KqQF/e7SIm", "2024-02-28 22:29:46.150639", "2024-02-28 22:29:46.150639");
+insert into statistika values (1, 15, "2024-02-28");
+insert into statistika values (2, 20, "2024-02-29");
